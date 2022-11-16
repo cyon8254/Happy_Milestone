58b829570ffd0cf4875fd763960852562b3a18f6
tjfla615 <tjfla615@gmail.com>
2022-11-17 01:38:07 +0900

    상세페이지 달력 js완료

--- milestone/src/main/resources/static/js/school/calendar.js
+++ milestone/src/main/resources/static/js/school/calendar.js
@@ -12,11 +12,16 @@ let month = todayMonth;
 let planDates = [];
 let count = 0;
 
-
 /* 오늘 날짜 */
 let todayAr = [today.getFullYear(), today.getMonth()+1, today.getDate()];
 let result = todayAr[0]+"년 "+todayAr[1] +"월 "+todayAr[2] +"일";
 planDates.push({ "planDay": result });
+
+/* 날짜 클릭 */
+/* 눌린 날짜 */
+let saveDate="";
+/* 날짜 플래그 */
+let checkC = false;
 
 createCallender();
 
@@ -43,6 +48,7 @@ function createScope() {
     $dateScope = $('.daysBackground');
 }
 
+
 /* 달력 생성 */
 function callender() {
     count = 0;
@@ -50,21 +56,51 @@ function callender() {
     firstDay = new Date(year, month - 1, 1).getDay();
     lastDate = new Date(year, month - 1, 0).getDate();
     thisDate = new Date(year, month, 0).getDate();
-    // pastDate = new Date(year, month, 0).getDate();
     $dateScope.css("background-color", "#fff");
     $nowMonth.text(year + '년 ' + month + '월');
 
+    let thisIsToday=0;
+    let thisIsMonth=0;
+    let thisIsYear=0;
+
+
+    /* 이번달 저장 */
+    thisIsMonth = today.getMonth()+1;
+
+    /* 이번연도 저장 */
+    thisIsYear = today.getFullYear();
+
     for (let i = 0; i < $dateScope.length; i++) {
+
+        ;
+
         /* 이전 달 숫자 생성 */
         if (i < firstDay) {
             $dateScope.eq(i).css("color", 'rgb(200, 201, 207)')
             $dateScope.eq(i).text(lastDate - (firstDay - 1) + i);
         }
+
         /* 현재 달 숫자 생성 */
         if (i >= firstDay && (count <= thisDate)) {
             count++;
             $dateScope.eq(i).css("color", '#303441')
             $dateScope.eq(i).text(count);
+
+
+            /* 저장된 날짜 뿌려주기 */
+            if(saveDate){
+                let savedYear = saveDate.split('년')[0];
+                let savedMonth = saveDate.split('년')[1].split('월')[0];
+                let savedDate = saveDate.split('년')[1].split('월')[1].split('일')[0];
+                if($dateScope.eq(i).text() ==savedDate){
+                    if(month==savedMonth){
+                        if(year==savedYear){
+                            $dateScope.eq(i).css({"background-color": "#ffd400"});
+                        }
+                    }
+                }
+            }
+
             /* 오늘 표시 */
             planDates.forEach(function (plan) {
                 let splitSpace = plan.planDay.replace(' ', '');
@@ -76,18 +112,30 @@ function callender() {
                     if (month == planMonth) {
                         if (year == planYear) {
                             $dateScope.eq(i).css({"color":"rgb(0 95 255)"});
+                            /* 오늘 일 index로 저장 */
+                            thisIsToday = i;
                         }
                     }
                 }
             })
         }
+
+
         /* 다음 달 숫자 생성 */
         if (count > thisDate) {
             nextCount++;
             $dateScope.eq(i).css("color", 'rgb(200, 201, 207)')
             $dateScope.eq(i).text(nextCount);
+            $dateScope.eq(i).css({"pointer-events":"none"});
+        }
+
+
+        /* 오늘날짜보다 이전날짜 선택 막아주기 위한 컬러*/
+        if (thisIsToday==0 && month<=thisIsMonth && year==thisIsYear){
+            $dateScope.eq(i).css("color", 'rgb(189 186 186)');
         }
     }
+
 }
 
 
@@ -130,79 +178,28 @@ $tds.hover(function () {
     $(this).find('.daysBackground').css("filter", "brightness(100%)");
 })
 
-
-
-
-
-/* 날짜 클릭 */
-/* 눌린 날짜 */
-let saveDate="";
-/* 날짜 플래그 */
-let checkC = false;
-
 $tds.on('click', function(){
     var str =year+"년"+month+"월"+this.innerText+"일";
+    thisIsMonth = today.getMonth()+1;
+    thisIsYear = today.getFullYear();
 
-    for(var i=0; i<42; i++){
-        if(parseInt($("td")[i].innerText)<$(this).find('td').index){
-            console.log($(this).find('td').index);
-            console.log($("td")[i]);
-        }
-    }
-    if(parseInt(this.innerText)<today.getDate()){
+    if($(this).find("div.daysBackground").css("color")==='rgb(189, 186, 186)' && month<=thisIsMonth && year==thisIsYear){
         alert("지난 날짜는 선택하실 수 없습니다");
     }else{
         if(saveDate=="" && !checkC){
-            /* 선택 */
             $(this).find(".daysBackground").css("background-color", "#ffd400");
             saveDate+=str;
             checkC = true;
-            // console.log('추가완료 : '+saveDate);
+            console.log('추가 : '+saveDate);
         }else if(saveDate===str && checkC){
-            /* 선택 해제 */
             $(this).find(".daysBackground").css("background-color", "transparent");
             saveDate="";
             checkC = false;
+            console.log('삭제 : '+saveDate);
         }else if(saveDate.length>0){
-            /* 날짜를 여러개 선택했을 때*/
             alert("한번에 하루만 선택하실 수 있습니다");
         }
     }
-    // console.log(saveDate);
 })
 
 
-/* $tds.on('click', function(){
-        var str =""+year+month+this.innerText;
-
-    // if(parseInt(this.innerText)<today.getDate() && month>today.getMonth){
-    if(parseInt(this.innerText)<today.getDate() || month>today.getMonth){
-        alert("지난 날짜는 선택하실 수 없습니다");
-    }else{
-        if(saveDate.length==0){
-            console.log('무조건추가');
-            $(this).find(".daysBackground").css("background-color", "#ffd400");
-            saveDate.push(str);
-        }else{
-            for (var i = 0; i < saveDate.length; i++) {
-                if(str !== saveDate[i]){
-                    $(this).find(".daysBackground").css("background-color", "#ffd400");
-                    saveDate.push(str);
-                    console.log(saveDate[i] +'와 겹치지 않아서 추가했어');
-                    break;
-                }else if(str===saveDate[i]){
-                    $(this).find(".daysBackground").css("background-color", "transparent");
-                    saveDate.splice(i,1);
-                    console.log('누른'+str+'과'+saveDate[i] +'이 겹쳐서 삭제했어');
-                }
-            }
-        countC++;
-        console.log(saveDate);
-        }
-    }
-})
- */
-/* 이전날짜 클릭막기 버그*/
-/* 이전달, 다음달 날짜클릭 막기 */
-/* month가 1자리 일경우 앞에 '0'붙여야 뿌려줄때 자릿수로 자를수있음 */
-/* 날짜 저장했으면 뿌려주기 */
