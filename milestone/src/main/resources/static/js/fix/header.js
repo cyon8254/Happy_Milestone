4e73967112fc7a7e831bc907b80fd1ac3afcd0c5
Tumpkin34 <tumpkin34@gmail.com>
2022-11-18 19:11:14 +0900

    로그인 모달 완성

--- milestone/src/main/resources/static/js/fix/header.js
+++ milestone/src/main/resources/static/js/fix/header.js
@@ -9,27 +9,54 @@ const yellow4 = $("#yellow4");
 
 
 // noinspection DuplicatedCode,DuplicatedCode
-$(document).ready(function ()
-{
-    $(navText1).hover(function(){
+$(document).ready(function () {
+    $(navText1).hover(function () {
         $(yellow1).show();
-    }, function() {
+    }, function () {
         $(yellow1).hide();
     });
-    $(navText2).hover(function(){
+    $(navText2).hover(function () {
         $(yellow2).show();
-    }, function() {
+    }, function () {
         $(yellow2).hide();
     });
-    $(navText3).hover(function(){
+    $(navText3).hover(function () {
         $(yellow3).show();
-    }, function() {
+    }, function () {
         $(yellow3).hide();
     });
-    $(navText4).hover(function(){
+    $(navText4).hover(function () {
         $(yellow4).show();
-    }, function() {
+    }, function () {
         $(yellow4).hide();
     });
 });
 
+/*----------------------------------------------*/
+/*--------------------황지수---------------------*/
+/*----------------------------------------------*/
+const $loginBtn = $('#loginBtn');
+const $loginModalWrap = $('.loginModalWrap');
+const $loginModalScope = $('.loginModalScope');
+const $closeBackground = $('.closeBackground');
+const $body = $('body');
+
+
+document.addEventListener('click', function (e) {
+    console.log(e.target)
+    console.log($loginModalScope[0])
+    if (e.target == $loginModalScope[0]) {
+        $loginModalWrap.hide();
+        $body.css('overflow', 'auto');
+    }
+})
+
+$loginBtn.on('click', function () {
+    $loginModalWrap.show();
+    $body.css('overflow', 'hidden');
+})
+
+$closeBackground.on('click', function () {
+    $loginModalWrap.hide();
+    $body.css('overflow', 'auto');
+})
